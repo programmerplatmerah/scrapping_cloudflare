163cb5c0f231075a592067682b3b1ff39d1d776d
Zipo Namberwan <dopokan@gmail.com>
2025-05-29 16:38:14 +0700

    fix the selenium

--- selenium to exisiting browser.py
+++ selenium to exisiting browser.py
@@ -3,28 +3,18 @@
 # "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --profile-directory="Profile 6"
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options  # Use Edge Options
 
-def connect_to_existing_chrome():
-    # Set up options to connect to existing browser
-    chrome_options = Options()
-    chrome_options.add_experimental_option("debuggerAddress", "localhost:9222")
-    
-    # Connect to the browser
-    driver = webdriver.Chrome(options=chrome_options)
-    
-    # Now you can control the existing browser
+def connect_to_existing_edge():
+    edge_options = Options()
+    edge_options.add_experimental_option("debuggerAddress", "localhost:9222")
+    driver = webdriver.Edge(options=edge_options)
+
     driver.get("https://www.google.com/search?q=playwright+by+testers+talk")
-    
-    # Find and click link (Selenium syntax differs from Playwright)
     links = driver.find_elements("xpath", "//a[contains(text(), 'Playwright by Testers Talk')]")
     if links:
         links[0].click()
-    
-    # Keep the driver connection open
     input("Press Enter to quit...")
-    
-    # Don't call driver.quit() if you want to keep the browser open
 
 if __name__ == "__main__":
-    connect_to_existing_chrome()
+    connect_to_existing_edge()
